1c8934cc336d17dd1b41875447b6bd6384f182e1
mniemiec <mniemiec@appriss.com>
2020-12-20 21:12:10 +0100

    Further implementation of django forms

--- car_prices_tool_website/car_prices_tool/forms.py
+++ car_prices_tool_website/car_prices_tool/forms.py
@@ -4,57 +4,113 @@ from django.db.models import Count
 
 
 class SearchCarForm(forms.Form):
-    make = forms.ModelChoiceField(queryset=Car.objects.values('make').annotate(entries=Count('make')))
+    make_choices = []
+    makes = Car.objects.values('make').distinct()
+    for make in makes:
+        make_choices.append((make["make"], f'{make["make"]} ({Car.objects.filter(make=make["make"]).count()})'))
+    make = forms.ChoiceField(widget=forms.RadioSelect, choices=make_choices)
     state_choices = [('used', 'Used'), ('new', 'New'), ('both', 'Both')]
     state = forms.ChoiceField(widget=forms.RadioSelect, choices=state_choices)
 
-
-class OptionalSearchCarForm(forms.Form):
     # Model:
-    model = forms.ModelChoiceField(
-        queryset=Car.objects.values('model').filter(make='Audi').annotate(entries=Count('model')))
+    model_choices = []
+    models = Car.objects.values('model').distinct()
+    for model in models:
+        model_choices.append((model["model"], f'{model["model"]} ({Car.objects.filter(model=model["model"]).count()})'))
+    model = forms.ChoiceField(widget=forms.RadioSelect, choices=model_choices)
 
     # Offer type:
     offer_type_choices = [('all', 'All'),
                           ('person', 'Person'),
                           ('company', 'Company')]
-    offer_type = forms.ChoiceField(widget=forms.RadioSelect, choices=offer_type_choices)
+    offer_type = forms.ChoiceField(widget=forms.RadioSelect, choices=offer_type_choices, required=False)
 
     # Mileage:
     mileage_less_more_choices = [('mileage_less_than', 'Equal or less than:'),
                                  ('mileage_more_than', 'Equal or more than:')]
-    mileage_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=mileage_less_more_choices)
-    mileage = forms.IntegerField()
+    mileage_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=mileage_less_more_choices, required=False)
+    mileage = forms.IntegerField(required=False)
 
     # Production year:
     production_year_less_more_choices = [('production_year_less_than', 'Equal or earlier than:'),
                                          ('production_year_more_than', 'Equal or later than:'),
                                          ('production_year_exact', 'Exact:')]
-    production_year_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=production_year_less_more_choices)
-    production_year = forms.IntegerField()
+    production_year_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=production_year_less_more_choices, required=False)
+    production_year = forms.IntegerField(required=False)
 
     # Price:
     price_less_more_choices = [('price_less_than', 'Equal or less than:'),
                                ('price_more_than', 'Equal or more than:')]
-    price_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=production_year_less_more_choices)
-    price = forms.IntegerField()
+    price_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=production_year_less_more_choices, required=False)
+    price = forms.IntegerField(required=False)
 
     # Price currency:
     price_currency_choices = [('pln', 'Currency: PLN'),
                               ('usd', 'Currency: USD'),
                               ('eur', 'Currency: EUR')]
-    price_currency = forms.ChoiceField(widget=forms.RadioSelect, choices=price_currency_choices)
+    price_currency = forms.ChoiceField(widget=forms.RadioSelect, choices=price_currency_choices, required=False)
 
     # Engine capacity:
     engine_capacity_choices = [('engine_capacity_less_than', 'Equal or less than:'),
                                ('engine_capacity_more_than', 'Equal or more than:'),
                                ('engine_capacity_equal', 'Exact:')]
-    engine_capacity_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=engine_capacity_choices)
-    engine_capacity = forms.IntegerField()
+    engine_capacity_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=engine_capacity_choices, required=False)
+    engine_capacity = forms.IntegerField(required=False)
 
     # Engine power:
     engine_power_choices = [('engine_power_less_than', 'Equal or less than:'),
                             ('engine_power_more_than', 'Equal or more than:'),
                             ('engine_power_equal', 'Exact:')]
-    engine_power_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=engine_power_choices)
-    engine_power = forms.IntegerField()
+    engine_power_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=engine_power_choices, required=False)
+    engine_power = forms.IntegerField(required=False)
+
+
+# class OptionalSearchCarForm(forms.Form):
+#     # Model:
+#     model = forms.ModelChoiceField(
+#         queryset=Car.objects.values('model').filter(make='Audi').annotate(entries=Count('model')))
+#
+#     # Offer type:
+#     offer_type_choices = [('all', 'All'),
+#                           ('person', 'Person'),
+#                           ('company', 'Company')]
+#     offer_type = forms.ChoiceField(widget=forms.RadioSelect, choices=offer_type_choices)
+#
+#     # Mileage:
+#     mileage_less_more_choices = [('mileage_less_than', 'Equal or less than:'),
+#                                  ('mileage_more_than', 'Equal or more than:')]
+#     mileage_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=mileage_less_more_choices)
+#     mileage = forms.IntegerField()
+#
+#     # Production year:
+#     production_year_less_more_choices = [('production_year_less_than', 'Equal or earlier than:'),
+#                                          ('production_year_more_than', 'Equal or later than:'),
+#                                          ('production_year_exact', 'Exact:')]
+#     production_year_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=production_year_less_more_choices)
+#     production_year = forms.IntegerField()
+#
+#     # Price:
+#     price_less_more_choices = [('price_less_than', 'Equal or less than:'),
+#                                ('price_more_than', 'Equal or more than:')]
+#     price_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=production_year_less_more_choices)
+#     price = forms.IntegerField()
+#
+#     # Price currency:
+#     price_currency_choices = [('pln', 'Currency: PLN'),
+#                               ('usd', 'Currency: USD'),
+#                               ('eur', 'Currency: EUR')]
+#     price_currency = forms.ChoiceField(widget=forms.RadioSelect, choices=price_currency_choices)
+#
+#     # Engine capacity:
+#     engine_capacity_choices = [('engine_capacity_less_than', 'Equal or less than:'),
+#                                ('engine_capacity_more_than', 'Equal or more than:'),
+#                                ('engine_capacity_equal', 'Exact:')]
+#     engine_capacity_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=engine_capacity_choices)
+#     engine_capacity = forms.IntegerField()
+#
+#     # Engine power:
+#     engine_power_choices = [('engine_power_less_than', 'Equal or less than:'),
+#                             ('engine_power_more_than', 'Equal or more than:'),
+#                             ('engine_power_equal', 'Exact:')]
+#     engine_power_less_more = forms.ChoiceField(widget=forms.RadioSelect, choices=engine_power_choices)
+#     engine_power = forms.IntegerField()
--- car_prices_tool_website/car_prices_tool/views.py
+++ car_prices_tool_website/car_prices_tool/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from car_prices_tool.models import Car
 from django.core import serializers
 from django.db.models import Count
-from car_prices_tool.forms import SearchCarForm, OptionalSearchCarForm
+from car_prices_tool.forms import SearchCarForm
 
 
 def home(request):
@@ -25,52 +25,60 @@ def search(request):
     models = Car.objects.values('model').annotate(entries=Count('model'))
 
     form = SearchCarForm
-    optional_form = OptionalSearchCarForm
+
+    if request.method == 'POST':
+        filled_form = SearchCarForm(request.POST)
+        if filled_form.is_valid():
+            context = {
+                'make': filled_form.cleaned_data['make'],
+                'state': filled_form.cleaned_data['state'],
+                'model': filled_form.cleaned_data['model'],
+                'offer_type': filled_form.cleaned_data['offer_type'],
+                'mileage_less_more': filled_form.cleaned_data['mileage_less_more'],
+                'mileage': filled_form.cleaned_data['mileage'],
+                'production_year_less_more': filled_form.cleaned_data['production_year_less_more'],
+                'production_year': filled_form.cleaned_data['production_year'],
+                'price_less_more': filled_form.cleaned_data['price_less_more'],
+                'price': filled_form.cleaned_data['price'],
+                'price_currency': filled_form.cleaned_data['price_currency'],
+                'engine_capacity_less_more': filled_form.cleaned_data['engine_capacity_less_more'],
+                'engine_capacity': filled_form.cleaned_data['engine_capacity'],
+                'engine_power_less_more': filled_form.cleaned_data['engine_power_less_more'],
+                'engine_power': filled_form.cleaned_data['engine_power']
+            }
+
+            return results(request, context)
 
     context = {
         'cars': cars,
         'makes': makes,
         'models': models,
-        'form': form,
-        'optional_form': optional_form
+        'form': form
     }
 
     return render(request, 'car_prices_tool/search.html', context)
 
 
-def results(request):
-    make = request.GET.get('make')
-    state = request.GET.get('state')
-    model = request.GET.get('model')
-    offer_type = request.GET.get('offer_type')
-    mileage_less_more = request.GET.get('mileage_less_more')
-    mileage = request.GET.get('mileage')
-    production_year_less_more = request.GET.get('production_year_less_more')
-    production_year = request.GET.get('production_year')
-    price_less_more = request.GET.get('price_less_more')
-    price = request.GET.get('price')
-    price_currency = request.GET.get('price_currency')
-    engine_capacity_less_more = request.GET.get('engine_capacity_less_more')
-    engine_capacity = request.GET.get('engine_capacity')
-    engine_power_less_more = request.GET.get('engine_power_less_more')
-    engine_power = request.GET.get('engine_power')
+def results(request, context):
+    make = context.get('make')
+    state = context.get('state')
+    model = context.get('model')
+    offer_type = context.get('offer_type')
+    mileage_less_more = context.get('mileage_less_more')
+    mileage = context.get('mileage')
+    production_year_less_more = context.get('production_year_less_more')
+    production_year = context.get('production_year')
+    price_less_more = context.get('price_less_more')
+    price = context.get('price')
+    price_currency = context.get('price_currency')
+    engine_capacity_less_more = context.get('engine_capacity_less_more')
+    engine_capacity = context.get('engine_capacity')
+    engine_power_less_more = context.get('engine_power_less_more')
+    engine_power = context.get('engine_power')
 
     context = {
-        'make': make,
-        'state': state,
-        'model': model,
-        'offer_type': offer_type,
-        'mileage_less_more': mileage_less_more,
-        'mileage': mileage,
-        'production_year_less_more': production_year_less_more,
-        'production_year': production_year,
-        'price_less_more': price_less_more,
-        'price': price,
-        'price_currency': price_currency,
-        'engine_capacity_less_more': engine_capacity_less_more,
-        'engine_capacity': engine_capacity,
-        'engine_power_less_more': engine_power_less_more,
-        'engine_power': engine_power
+        'make': context.get('make'),
+        'state': context.get('state')
     }
 
     return render(request, 'car_prices_tool/results.html', context)
