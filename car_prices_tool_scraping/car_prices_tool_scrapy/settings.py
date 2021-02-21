de7d5de25e0e154b9df1b7fd93091bfcf2fd450f
mniemiec <michal.niemiec.it@gmail.com>
2021-02-21 16:59:01 +0100

    Fixes in spider, moved docker file

--- car_prices_tool_scraping/car_prices_tool_scrapy/settings.py
+++ car_prices_tool_scraping/car_prices_tool_scrapy/settings.py
@@ -10,12 +10,12 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 10
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 
 # between 0.5 * DOWNLOAD_DELAY and 1.5 * DOWNLOAD_DELAY
 RANDOMIZE_DOWNLOAD_DELAY = True
@@ -24,7 +24,7 @@ ROTATING_PROXY_LIST_PATH = 'car_prices_tool_scrapy/http_proxies.txt'
 USER_AGENT_LIST = 'car_prices_tool_scrapy/user_agents.txt'
 
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 10
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
@@ -49,8 +49,8 @@ USER_AGENT_LIST = 'car_prices_tool_scrapy/user_agents.txt'
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     'car_prices_tool_scrapy.middlewares.CarPricesToolScrapyDownloaderMiddleware': 543,
-    # 'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-    # 'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
     'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
     'random_useragent.RandomUserAgentMiddleware': 400
 }
@@ -64,20 +64,19 @@ DOWNLOADER_MIDDLEWARES = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'car_prices_tool_scrapy.pipelines.CarPricesToolScrapyPipeline': 300,
-   'car_prices_tool_scrapy.pipelines.JsonPipeline': 900,
+    'car_prices_tool_scrapy.pipelines.JsonPipeline': 900,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 3.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
--- car_prices_tool_scraping/car_prices_tool_scrapy/spiders/CARS_PL_source_1.py
+++ car_prices_tool_scraping/car_prices_tool_scrapy/spiders/CARS_PL_source_1.py
@@ -1,12 +1,11 @@
-import os
 import sys
+import re
+import os
 import json
 import inspect
 import pendulum
 from scrapy import spiders
-# from car_prices_tool_scrapy.items import Car
 from scrapy.loader import ItemLoader
-
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
@@ -20,9 +19,17 @@ class CARSPLsource1Spider(spiders.CrawlSpider):
     def parse_start_url(self, response):
         data = json.loads(response.xpath('//script[@id="__NEXT_DATA__"]/text()').get())
         makes = data['props']['pageProps']['filtersValues']['571'][1]['group_values']
-        for make in makes[:1]:
-            make = make['search_key']
-            url = f'https://www.source1.pl/osobowe/uzywane/{make}/'
+
+        # For loop to scrape used cars.
+        for make in makes:
+            url = f'https://www.source1.pl/osobowe/uzywane/{make["search_key"]}/'
+
+            yield response.follow(url, callback=self.parse_cars)
+
+        # For loop to scrape new cars.
+        for make in makes:
+            url = f'https://www.source1.pl/osobowe/nowe/{make["search_key"]}/'
+
             yield response.follow(url, callback=self.parse_cars)
 
     def parse_cars(self, response):
@@ -37,34 +44,49 @@ class CARSPLsource1Spider(spiders.CrawlSpider):
             yield response.follow(next_page, callback=self.parse_cars)
 
     def parse_car_detail(self, response):
-        make = response.xpath('//*[contains(text(), "Marka pojazdu")]/following::*[1]/a/text()').get('')
-        model = response.xpath('//*[contains(text(), "Model pojazdu")]/following::*[1]/a/text()').get('')
-        if model == 'Inny':
-            model = ''
-        model_variant = response.xpath('//*[contains(text(), "Wersja")]/following::*[1]/a/text()').get('')
-        production_year = response.xpath('//*[contains(text(), "Rok produkcji")]/following::*[1]/text()').get('')
+        make = response.xpath('//*[contains(text(), "Marka pojazdu")]/following::*[1]/a/text()').get()
+
+        model = response.xpath('//*[contains(text(), "Model pojazdu")]/following::*[1]/a/text()').get()
+        if model and 'Inny' in model:
+            model = 'Other'
+
+        model_variant = response.xpath('//*[contains(text(), "Wersja")]/following::*[1]/a/text()').get()
+        production_year = response.xpath('//*[contains(text(), "Rok produkcji")]/following::*[1]/text()').get()
         engine_power = response.xpath('//*[contains(text(), "Moc")]/following::*[1]/text()').re_first(r'(.+) KM')
+
         mileage = response.xpath('//*[contains(text(), "Przebieg")]/following::*[1]/text()').re_first(r'(.+) km')
-        mileage = int(mileage.replace(' ', ''))
+        if mileage:
+            mileage = int(mileage.replace(' ', ''))
+
         engine_capacity = response.xpath('//*[contains(text(), "Pojemność skokowa")]/following::*[1]/text()').re_first(r'(.+) cm3')
         # Get format of engine_capacity as float 1.9 or 2.3
         if engine_capacity:
             engine_capacity = '%.1f' % float(int(engine_capacity.replace(' ', ''))/1000)
-        offer_type = response.xpath('//*[contains(text(), "Oferta od")]/following::*[1]/a/text()').get('')
+
+        offer_type = response.xpath('//*[contains(text(), "Oferta od")]/following::*[1]/a/text()').get()
         if 'Osoby prywatnej' in offer_type:
             offer_type = 'person'
         elif 'Firmy' in offer_type:
             offer_type = 'business'
+
         price = response.xpath('//div[@class="offer-price changeFinanceLinkOrder"]/span/text()').get()
         price_currency = response.xpath('//div[@class="offer-price changeFinanceLinkOrder"]/span/span/text()').get()
-        state = response.xpath('//*[contains(text(), "Stan")]/following::*[1]/a/text()').get('')
-        if 'Używane' in state:
+
+        state = response.xpath('//*[contains(text(), "Stan")]/following::*[1]/a/text()').get()
+        if state and 'Używane' in state:
             state = 'Used'
-        elif 'Nowe' in state:
+        elif state and 'Nowe' in state:
             state = 'New'
+
         damaged = response.xpath('//*[contains(text(), "Uszkodzony")]/following::*[1]/a/text()').get()
         if damaged:
-            damaged = 'True'
+            damaged = True
+
+        date_issued = response.xpath('//span[@class="offer-meta__value"]/text()').re_first(r', (.+)')
+        day = re.findall(r'(^\d+)|$', date_issued)[0]
+        month = self.get_month(date_issued)
+        year = re.findall(r'(\d{4})|$', date_issued)[0]
+        date_issued = f'{day}/{month}/{year}'
 
         # Item loader
         car = ItemLoader(item=Car(), selector=response)
@@ -79,7 +101,38 @@ class CARSPLsource1Spider(spiders.CrawlSpider):
         car.add_value('price', price)
         car.add_value('price_currency', price_currency)
         car.add_value('state', state)
-        car.add_value('date_scraped', pendulum.now('CET'))
+        car.add_value('date_scraped', pendulum.now('CET').format('DD/MM/YYYY'))
+        car.add_value('date_issued', date_issued)
+        car.add_value('damaged', damaged)
 
-        if not damaged:
-            return car.load_item()
+        return car.load_item()
+
+    @staticmethod
+    def get_month(date_issued):
+        month = re.findall(r'[a-zA-Zęóąśłżźćń]+|$', date_issued)[0]
+        if 'stycz' in month:
+            month = '01'
+        elif 'lut' in month:
+            month = '02'
+        elif 'mar' in month:
+            month = '03'
+        elif 'kwie' in month:
+            month = '04'
+        elif 'maj' in month:
+            month = '05'
+        elif 'czerw' in month:
+            month = '06'
+        elif 'lip' in month:
+            month = '07'
+        elif 'sierp' in month:
+            month = '08'
+        elif 'wrze' in month:
+            month = '09'
+        elif 'październik' in month:
+            month = '10'
+        elif 'listopad' in month:
+            month = '11'
+        elif 'grud' in month:
+            month = '12'
+
+        return month
